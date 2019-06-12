1160f6b5bec28e7a038ca2f2376707ad12e81115
David Marchante <cdmarchante@gmail.com>
2019-06-12 11:04:27 -0700

    add route functionality and test

---
+++ songr/src/main/java/com/dmarchante/code401d4/class12/songr/Songr.java
@@ -0,0 +1,14 @@
+package com.dmarchante.code401d4.class12.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Songr {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Songr.class, args);
+	}
+
+
+}
